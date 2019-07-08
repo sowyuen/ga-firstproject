2932d31ecde2c27c307caa4bbbb0a76f890c047e
sowyuen <sowyuenl@gmail.com>
2019-07-09 01:08:51 +0800

    3rd commit: check inputWords letter by letter  input box turn red if one letter type wrongly  turns green if type correctly input trim() for inputWords

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-window.onload = function(){
-    init();;
-}
-
 // global variable
 let time;
 let timeLevel;
 let score = 0;
-let playing;
+let previousSelectedIndex;
 
 //grab DOM elements
 const wordInput = document.querySelector("#word-input");
 const scoreDisplay = document.querySelector("#scoreGained");
 const timeDisplay = document.querySelector("#secondsLeft");
 const messageDisplay = document.querySelector("#gameResults");
-const secondShownOnTop = document.querySelector("#seconds");
 const circular = document.querySelector("#circular");
 var currentWordDisplay = null;
+
 //setting animations
 const secondsDisplay = document.querySelector("#secondsLeft");
-secondsLeft.classList.add('animated','heartBeat','infinite');
+secondsDisplay.classList.add('animated','heartBeat','infinite');
 const gameMessage =  document.querySelector('#gameResults');
-gameResults.classList.add('animated', 'tada');
+gameMessage.classList.add('animated', 'tada');
 
 //buttons to choose level
 const easyButton = document.querySelector("#easy");
@@ -28,74 +24,161 @@ const mediumButton = document.querySelector("#medium");
 const hardButton = document.querySelector("#hard");
 
 //arrays of words
-var words = [
+var wordsArray = [
 'snowball',
 'roof',
-'fly',
 'bicycle',
-'bear',
-'cape',
 'puppet',
 'piano',
 'lipstick',
 'salute',
 'penguin',
 'banana',
-'peel',
 'whisper',
 'popsicle',
 'frankenstein',
 'earthquake',
-'road',
-'rain',
-'alarm',
-'clock',
-'dog',
 'leash',
 'pajamas',
-'slam',
 'fiddle',
 'seashell',
 'seesaw',
 'cheerleader',
 'shopping',
 'newspaper',
-'twist',
 'rhinoceros',
-'cow',
 'tickle',
-'fetch',
 'violin',
 'cage',
-'braid',
 'skateboard',
 'stairs',
 'trumpet',
-'mop',
 'shovel',
 'money',
-'soap',
 'saddle',
-'wink',
 'tree',
 'spider',
-'man',
 'think',
 'pizzas',
 'suburban',
 'assuming',
 'obstinance',
 'foramens',
-'general'
+'general',
+'Singapore',
+'Thailand',
+'China',
+'pastoral',
+'carpenter',
+'rabbits',
+'sack',
+'brainy',
+'report',
+'suck',
+'snore',
+'soup',
+'cherry',
+'panoramic',
+'brass',
+'achievements',
+'preparations',
+'animations',
+'emblems',
+'encouragement',
+'recognition',
+'p@ssw0rd',
+'p@ssc0de',
+'popularity',
+'eliminations',
+'juggernaut',
+'airborne',
+'fridge',
+'reliability',
+'emotions',
+'definitely',
+'government',
+'publicly',
+'receive',
+'politician',
+'bookkeeper',
+'hippopo',
 ];
 
-//initialize the game
-function init(){
-    console.log("hello initializing");
-    //choosing level
-    easyButton.addEventListener("click",chooseLevel);
-    mediumButton.addEventListener("click",chooseLevel);
-    hardButton.addEventListener("click",chooseLevel);
+//To show random words
+function showWord(words){
+    //To generate random words
+    const randomIndex = Math.floor(Math.random() * words.length);
+    // console.log(randomIndex);
+    //so that output random word will not repeat
+    if(previousSelectedIndex !== randomIndex){
+    currentWordDisplay =  document.createElement('p');
+    currentWordDisplay.innerHTML = words[randomIndex];
+    //set animations
+    currentWordDisplay.setAttribute("id","current-word");
+    currentWordDisplay.classList.add('animated', 'bounceIn');
+    circular.appendChild(currentWordDisplay);
+    previousSelectedIndex = randomIndex;
+} else{
+    showWord(words);
+    }
+}
+
+function checkWord(){
+    console.log(wordInput.value);
+    //even if there is spacing still okay
+    wordInput.value = wordInput.value.trim();
+    if (wordInput.value === currentWordDisplay.innerHTML){
+        //if not 2 words stuck
+        circular.removeChild(currentWordDisplay);
+        showWord(wordsArray);
+        wordInput.value = "";
+        //whole word correct
+        messageDisplay.innerHTML= "CORRECT!";
+        scoreDisplay.innerHTML= score;
+        score++;
+        time=timeLevel+1;
+    }
+
+    else{
+        //checking letter by letter if its correct
+        const inputLength = wordInput.value.length;
+        wordInput.style.backgroundColor = 'green';
+        messageDisplay.innerHTML = "CORRECT";
+        console.log(inputLength);
+        console.log('arrayWord', currentWordDisplay.textContent);
+        console.log('typedWord', wordInput.value);
+
+//taking the first letter and up to the length
+    if(currentWordDisplay.innerHTML.slice(0,inputLength)!== wordInput.value){
+        wordInput.style.backgroundColor = "red";
+        messageDisplay.textContent = "WRONG!";
+    }
+}
+
+    // if score is -1, display 0
+    if(score === -1){
+        scoreDisplay.innerHTML = 0;
+    }
+    else{
+    scoreDisplay.innerHTML = score;
+    }
+}
+
+
+    //to count
+function timer(){
+    //check if there is time
+    if(time>0){
+        //decrease the time
+        time--;
+        //show time
+        timeDisplay.innerHTML = time;
+    }
+    else if(time===0){
+        //game is over!
+        messageDisplay.innerHTML = "GAME OVER!!!!";
+        score=-1;
+    }
 }
 
 function chooseLevel(event){
@@ -117,69 +200,24 @@ function chooseLevel(event){
 
 function startGame(){
     // load random words from array
-    showWord(words);
+    showWord(wordsArray);
     //match word input
     wordInput.addEventListener('keyup',checkWord);
     wordInput.focus();
     //call timer
     time=timeLevel +1;
     setInterval(timer,1000);
-
 }
 
-//To show random words
-function showWord(words){
-    //To generate random words
-    const randomIndex = Math.floor(Math.random() * words.length);
-    // console.log(randomIndex);
-    //output random word
-    currentWordDisplay =  document.createElement('p');
-    currentWordDisplay.innerHTML = words[randomIndex];
-    //set animations
-    currentWordDisplay.setAttribute("id","current-word");
-    currentWordDisplay.classList.add('animated', 'bounceIn');
-    circular.appendChild(currentWordDisplay);
+//initialize the game
+function init(){
+    console.log("hello initializing");
+    //choosing level
+    easyButton.addEventListener("click",chooseLevel);
+    mediumButton.addEventListener("click",chooseLevel);
+    hardButton.addEventListener("click",chooseLevel);
+}
 
-    //currentWord.style.opacity = 1;
-}
-function checkWord(){
-    console.log(wordInput.value);
-    if (wordInput.value === currentWordDisplay.innerHTML){
-        //if not 2 words stuck
-        circular.removeChild(currentWordDisplay);
-        playing= true;
-        showWord(words);
-        wordInput.value = "";
-        messageDisplay.innerHTML= "CORRECT!";
-        score++;
-        time=timeLevel+1;
-        // return true;
-    }
-    else{
-        scoreDisplay.innerHTML= score;
-        messageDisplay.innerHTML="WRONG!";
-
-    }
-    // if score is -1, display 0
-    if(score === -1){
-        scoreDisplay.innerHTML = 0;
-    }
-    else{
-    scoreDisplay.innerHTML = score;
-    }
-}
-//to count
-function timer(){
-    //check if there is time
-    if(time>0){
-        //decrease the time
-        time--;
-        //show time
-        timeDisplay.innerHTML = time;
-    }
-    else if(time===0){
-        //game is over!
-        playing = false;
-        messageDisplay.innerHTML = "GAME OVER!!!!";
-    }
-}
+window.onload = function() {
+  init();
+};
